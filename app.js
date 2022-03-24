c32e85d84d932f1bdd36cb1fd6f49f34721b9498
orhanurullah <orhann.1512@gmail.com>
2022-03-24 13:01:00 +0300

    Make some changes

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 const blog = {
     id:1,
     title:"Blog Title",
     description:"The perfect posts"
 };
 app.get('/', (req,res) => {
-    res.status(200).send(blog);
+    res.render('index');
 });
-
+app.get('/about', (req,res) => {
+    res.render('about');
+});
+app.get('/add-post', (req,res) => {
+    res.render('add_post');
+});
+app.get('/post', (req,res) => {
+    res.render('post');
+});
 const port = 3000;
 app.listen(port, () => {
     console.log(`Clean Blog starts on ${port} port`);
